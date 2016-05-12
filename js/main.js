0b6899c3cb0172edc3282dc7b53ced214976576b
kevinjsheen <kevinjsheen@gmail.com>
2016-05-12 18:06:04 -0400

    First Take

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
 /* Custom JS goes here. */
-
+$(document).on('ready', function() {
+	
+$('#order-form').validate({
+	submitHandler: function(form) {
+		form.submit(); 
+	},
+	rules: {
+		"your-name"
+		"your-state"
+		"your-zip"
+		""
+	}
+	
+	
+	
 // For this assignment you'll need to do a few things:
 // 1. Create a document ready handler.
 // 2. Define a validation object for use on your page.
 // 3. Connect the validation object to an event handler tied to the submit button.
 
 // Refer to the `index.html` file for the validation rules that must be enforced.
+}); 
