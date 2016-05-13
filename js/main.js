52451136dc076237d45a067d468db6c9f02bf754
kevinjsheen <kevinjsheen@gmail.com>
2016-05-13 01:56:00 -0400

    Finished Requirements for JS and HTML

--- js/main.js
+++ js/main.js
@@ -6,14 +6,51 @@ $('#order-form').validate({
 		form.submit(); 
 	},
 	rules: {
-		"your-name"
-		"your-state"
-		"your-zip"
-		""
-	}
+		"your-name": {
+			required: true,
+			maxlength: 128
+		},
+		
+		"your-state": {
+			required: true,
+			maxlength: 2
+		},
+		
+		"your-zip": {
+			required: true,
+			maxlength: 5,
+			digits: true
+		},
+		
+		"card-holder-name": {
+			required: true,
+			maxlength: 128
+		}, 
 	
-	
-	
+		"card-number": {
+			required: true,
+			maxlength: 16,
+			creditcard: true
+		},
+		
+		"expiry-month": {
+			required: true
+		},		
+		
+		"cvv": {
+			required: true,
+			maxlength: 3,
+			digits: true
+		},
+		"shipping-method": {
+			required: true
+		},
+		"comments": {
+			required: false,
+			maxlength: 500
+		}
+	  }
+	});
 // For this assignment you'll need to do a few things:
 // 1. Create a document ready handler.
 // 2. Define a validation object for use on your page.
